f263bc80b99f35d41108f7e08cb91f471e83d88b
Slowaq <wbaluchaw@gmail.com>
2025-03-17 17:31:38 +0100

    Improved coding style (decomposed realocation)

--- 4_cvicenie/b3b36prg-hw03/main.c
+++ 4_cvicenie/b3b36prg-hw03/main.c
@@ -10,15 +10,14 @@
 enum{
   STARTING_LENGTH = 20,
   LETTERS_IN_ALPHABET = 26,
-  JUMP = 6,  // Space between Z and a in ASCII Table
-  RIGHT_BRACKET = 91 // ASCII position of [
 };
 
-void decipher(const char *str1, const char *str2);
-void shift(const char *str1, char* output, int offset);
-char rotate(char ch, int offset);
-int compare(const char *str1, const char *str2);
 char* read_input(int *lenght);
+char* realocate(char str[], int* capacity);
+void decipher(const char str1[], const char str2[]);
+void shift(const char str1[], char output[], int offset);
+char rotate(char ch, int offset);
+int compare(const char str1[], const char str2[]);
 
 int main(int argc, char *argv[])
 {
@@ -26,10 +25,7 @@ int main(int argc, char *argv[])
   int echo_length;
 
   char* cipher = read_input(&cipher_length);
-  if (!cipher) return ERROR;
-
   char* echo = read_input(&echo_length);
-  if (!echo) return ERROR;
 
   // Check for the same length
   if (cipher_length != echo_length){
@@ -64,18 +60,8 @@ char* read_input(int *lenght){
 
   do{
     // Realocate to a new memory if full
-    if (pos >= capacity){
-      capacity *= 2;
-      char* temp_buffer = realloc(buffer, capacity * sizeof(char));
-
-      if (temp_buffer == NULL){
-        fprintf(stderr, "Memory Reallocation Failed");
-        free(buffer);
-        exit (ERROR);
-      }
-
-      buffer = temp_buffer;
-    }
+    if (pos >= capacity) buffer = realocate(buffer, &capacity);
+  
     scanf("%c", &ch);
 
     if (ch == '\n'){
@@ -96,15 +82,26 @@ char* read_input(int *lenght){
   *lenght = pos;
   buffer[pos] = '\0'; // Null terminate
 
-
   return buffer;
 }
 
-void decipher(const char *str1, const char *str2){
+char* realocate(char str[], int *capacity){
+  *capacity *= 2;
+  char* temp_buffer = realloc(str, *capacity * sizeof(char));
+
+  if (temp_buffer == NULL){
+    fprintf(stderr, "Memory Reallocation Failed");
+    free(str);
+    exit (ERROR);
+  }
+
+  return temp_buffer;
+}
+
+void decipher(const char str1[], const char str2[]){
   int offset = 0;
   int best = 0;
   char shifted_cipher[strlen(str1) + 1]; // Magic number
-
 
   for (int i = 0; i < LETTERS_IN_ALPHABET * 2; ++i){  // Is 2 considered a magic number??
     shift(str1, shifted_cipher, i);
@@ -120,7 +117,7 @@ void decipher(const char *str1, const char *str2){
   printf("%s\n", shifted_cipher);
 }
 
-void shift(const char *str1, char* output, int offset){
+void shift(const char str1[], char output[], int offset){
   int len_cipher = strlen(str1);
   //char temp[len_cipher + 1];
 
@@ -142,17 +139,13 @@ char rotate(char ch, int offset) {
   return ch;  // Non-alphabetic characters remain unchanged
 }
 
-
-
-int compare(const char *str1, const char *str2){
+int compare(const char str1[], const char str2[]){
   int len_cipher = strlen(str1);
   int count = 0;
 
   for (int i = 0; i < len_cipher; ++i){
-    // printf("%c,%c ", str1[i], str2[i]);
     if (str1[i] == str2[i] && (str1[i] != '\0' || str2[i] != '\0'))
       count += 1;
   }
-  // printf("%d\n", count);
   return count;
 }
